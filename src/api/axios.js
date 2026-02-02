19531663f482b428582358a5e7d20f0d78a96d66
Your Name <Emmanuelaquarius2006@gmail.com>
2026-02-02 22:33:29 +0000

    fetching datat from backend is added  using env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
+const API_URL = import.meta.env.VITE_PORT_URL
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: API_URL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
